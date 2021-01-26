3e0dd5a8f055d3686689498f48c93cafb9f7def4
Portgate Studios <info@portgatestudios.com>
2021-01-26 09:32:22 +0100

    feat: Mailer added, also GMailer with gmail setup already.

---
+++ Mailer/GMailer.cs
@@ -0,0 +1,28 @@
+namespace Mailer
+{
+	public class GMailerOptions
+	{
+		public string Name;
+		public string Username;
+		public string Password;
+	}
+
+	public static class GMailer
+	{
+		public static void Send(GMailerOptions gMailerOptions, string recipient, string subject, string body)
+		{
+			var mailerOptions = new MailerOptions()
+			{
+				SmtpHost = "smtp.gmail.com",
+				SmtpPort = 587,
+				EnableSsl = true,
+				IsBodyHtml = true,
+				Name = gMailerOptions.Name,
+				Email = $"{gMailerOptions.Username}@gmail.com",
+				Password = gMailerOptions.Password
+			};
+
+			GenericMailer.Send(mailerOptions, recipient, subject, body);
+		}
+	}
+}
---
+++ Mailer/Mailer.cs
@@ -0,0 +1,40 @@
+using System.Net;
+using System.Net.Mail;
+
+namespace Mailer
+{
+	public class MailerOptions
+	{
+		public string SmtpHost;
+		public int SmtpPort;
+		public bool EnableSsl;
+		public bool IsBodyHtml;
+		public string Name;
+		public string Email;
+		public string Password;
+	}
+
+	public static class GenericMailer
+	{
+		public static void Send(MailerOptions mailerOptions, string recipient, string subject, string body)
+		{
+			var client = new SmtpClient(mailerOptions.SmtpHost, mailerOptions.SmtpPort)
+			{
+				Credentials = new NetworkCredential(mailerOptions.Email, mailerOptions.Password),
+				EnableSsl = mailerOptions.EnableSsl,
+			};
+
+			var msg = new MailMessage()
+			{
+				From = new MailAddress(mailerOptions.Email, mailerOptions.Name),
+				Subject = subject,
+				Body = body,
+				IsBodyHtml = mailerOptions.IsBodyHtml
+			};
+
+			msg.To.Add(recipient);
+
+			client.Send(msg);
+		}
+	}
+}
