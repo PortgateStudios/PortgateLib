5bd6d5799380cd4311fbaf98bef06e94d1b183c3
Portgate Studios <info@portgatestudios.com>
2021-02-20 13:35:29 +0100

    feat(ImageWebLoader): component to load Image.sprites via urls.

---
+++ ImageWebLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using PortgateLib.Downloader;
+
+namespace PortgateLib
+{
+	[RequireComponent(typeof(Image))]
+	public class ImageWebLoader : MonoBehaviour
+	{
+		private Action onSuccess;
+
+		public void StartLoadingImage(string url, Action onSuccess = null, Action<string> onFail = null)
+		{
+			this.onSuccess = onSuccess;
+			StartCoroutine(TextureDownloader.StartDownload(url, OnTextureDownloaded, onFail));
+		}
+
+		private void OnTextureDownloaded(Texture2D texture)
+		{
+			var rect = new Rect(0, 0, texture.width, texture.height);
+			var sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f), 100);
+			GetComponent<Image>().sprite = sprite;
+			if (onSuccess != null)
+			{
+				onSuccess();
+			}
+		}
+	}
+}
