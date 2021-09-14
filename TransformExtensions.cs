eed65e2136b40fce5583e5194506d12309527a0c
Portgate Studios <info@portgatestudios.com>
2021-09-14 10:30:15 +0200

    feat: TransformExtensions for getting needed rotation and checking if it's looking towards a point

---
+++ TransformExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace PortgateLib
+{
+	public static class TransformExtensions
+	{
+		public static float GetNeededRotationTo(this Transform transform, Vector3 point)
+		{
+			var lookingDirection = transform.position.GetDirectionTo(point);
+			var neededRotation = Vector2.SignedAngle(transform.up, lookingDirection);
+			return neededRotation;
+		}
+
+		public static bool IsLookingTowardsPoint(this Transform transform, Vector3 point, float epsilon = 0f)
+		{
+			var neededRotation = transform.GetNeededRotationTo(point);
+			var delta = Mathf.Abs(neededRotation);
+			return Mathf.Approximately(Mathf.Max(0, delta - epsilon), 0);
+		}
+	}
+}
