23938768de7a0b7bd49511041756a591c04fcf8e
Portgate Studios <info@portgatestudios.com>
2022-05-05 19:37:26 +0200

    feat(ColliderExtensions): wip Collider.OverlapCollider(). CreateSphere, GetWidth and GetHeight for 3D colliders.

--- ColliderExtensions.cs
+++ ColliderExtensions.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace PortgateLib
 {
 	public static class ColliderExtensions
 	{
+		public static Collider[] OverlapCollider(this Collider collider, Transform transform)
+		{
+			var type = collider.GetType();
+			if (type == typeof(BoxCollider))
+			{
+				var boxCollider = collider as BoxCollider;
+				var center = transform.TransformPoint(boxCollider.center);
+				var sizes = boxCollider.size;
+				var scale = transform.localScale;
+				sizes.Scale(scale);
+				return Physics.OverlapBox(center, sizes / 2f, transform.rotation);
+			}
+			else if (type == typeof(SphereCollider))
+			{
+				var sphereCollider = collider as SphereCollider;
+				(var center, var radius) = sphereCollider.GetSphereData(transform);
+				return Physics.OverlapSphere(center, radius);
+			}
+			else if (type == typeof(CharacterController))
+			{
+				var characterController = collider as CharacterController;
+				throw new Exception("NOT YET IMPLEMENTED BECAUSE I DON'T NEED IT YET AND IT TAKES TIME TO FIGURE IT OUT");
+			}
+			else
+			{
+				throw new Exception("Collider type is not handled!");
+			}
+		}
+
+		public static GameObject CreateSphere(this SphereCollider collider, string name, Transform transform)
+		{
+			var sphereCollider = collider as SphereCollider;
+			(var center, var radius) = sphereCollider.GetSphereData(transform);
+			return DebugUtility.CreateSphere(name, center, radius);
+		}
+
 		public static float GetWidth(this Collider2D collider, Transform transform)
 		{
 			var type = collider.GetType();
@@ -31,6 +65,61 @@ namespace PortgateLib
 			{
 				throw new Exception("Collider type is not handled!");
 			}
+		}
+
+		public static float GetWidth(this Collider collider, Transform transform)
+		{
+			var type = collider.GetType();
+			if (type == typeof(BoxCollider))
+			{
+				var boxCollider = collider as BoxCollider;
+				var width = boxCollider.size.x * transform.localScale.x;
+				var depth = boxCollider.size.z * transform.localScale.z;
+				return Mathf.Max(width, depth);
+			}
+			else if (type == typeof(SphereCollider))
+				return (collider as SphereCollider).radius * 2 * GetSphereScale(transform.localScale);
+			else if (type == typeof(CharacterController))
+				return (collider as CharacterController).radius * 2 * GetHorizontalCapsuleScale(transform.localScale);
+			else
+			{
+				throw new Exception("Collider type is not handled!");
+			}
+		}
+
+		public static float GetHeight(this Collider collider, Transform transform)
+		{
+			var type = collider.GetType();
+			if (type == typeof(BoxCollider))
+				return (collider as BoxCollider).size.y * transform.localScale.y;
+			else if (type == typeof(SphereCollider))
+				return (collider as SphereCollider).radius * 2 * GetSphereScale(transform.localScale);
+			else if (type == typeof(CharacterController))
+				return (collider as CharacterController).height * transform.localScale.y;
+			else
+			{
+				throw new Exception("Collider type is not handled!");
+			}
+		}
+
+		private static (Vector3 center, float radius) GetSphereData(this SphereCollider collider, Transform transform)
+		{
+			var center = transform.TransformPoint(collider.center);
+			var scale = GetSphereScale(transform.localScale);
+			var radius = collider.radius * scale;
+			return (center, radius);
+		}
+
+		private static float GetSphereScale(Vector3 scale)
+		{
+			// this is how Unity handles sphere colliders' scaling.
+			return Mathf.Max(scale.x, Mathf.Max(scale.y, scale.z));
+		}
+
+		private static float GetHorizontalCapsuleScale(Vector3 scale)
+		{
+			// this is how Unity handles capsule colliders' scaling.
+			return Mathf.Max(scale.x, scale.z);
 		}
 	}
 }
