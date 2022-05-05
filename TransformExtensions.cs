47b055b2734849bf035a7c2840584308bfff8c6b
Portgate Studios <info@portgatestudios.com>
2022-05-05 14:31:31 +0200

    feat(TransformExtensions): Turned them into Vector2 using methods because in an isometric game we need Vector2, using the Vector3 ones caused bugs.

--- TransformExtensions.cs
+++ TransformExtensions.cs
@@ -4,17 +4,17 @@ namespace PortgateLib
 {
 	public static class TransformExtensions
 	{
-		public static bool IsLookingTowardsPoint(this Transform transform, Vector3 point, float epsilon = 0f)
+		public static bool IsLookingTowardsPoint(this Transform transform, Vector2 point, float epsilon = 0f)
 		{
 			var neededRotation = transform.GetNeededRotationTo(point);
 			var delta = Mathf.Abs(neededRotation);
 			return Mathf.Approximately(Mathf.Max(0, delta - epsilon), 0);
 		}
 
-		public static float GetNeededRotationTo(this Transform transform, Vector3 point)
+		public static float GetNeededRotationTo(this Transform transform, Vector2 point)
 		{
-			var lookingDirection = transform.position.GetDirectionTo(point);
-			var neededRotation = Vector2.SignedAngle(transform.up, lookingDirection);
+			var lookingDirection = transform.position.ToVector2XZ().GetDirectionTo(point);
+			var neededRotation = Vector3.SignedAngle(transform.forward, lookingDirection.ToVector3XZ(), Vector3.up);
 			return neededRotation;
 		}
 	}
