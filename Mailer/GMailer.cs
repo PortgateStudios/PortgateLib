35992a1c994734d6e992cbdae640d0a5fd921c3d
Portgate Studios <info@portgatestudios.com>
2023-08-11 22:39:01 +0200

    feat(GMailer): SendAsync

--- Mailer/GMailer.cs
+++ Mailer/GMailer.cs
@@ -1,17 +1,31 @@
+using System.Threading.Tasks;
+
 namespace PortgateLib.Mailer
 {
-	public class GMailerOptions
+	public record GMailerOptions
 	{
-		public string Name;
-		public string Username;
-		public string Password;
+		public string Name { get; init; }
+		public string Username { get; init; }
+		public string Password { get; init; }
 	}
 
 	public static class GMailer
 	{
 		public static void Send(GMailerOptions gMailerOptions, string recipient, string subject, string body)
 		{
-			var mailerOptions = new MailerOptions()
+			var mailerOptions = GetMailerOptions(gMailerOptions);
+			GenericMailer.Send(mailerOptions, recipient, subject, body);
+		}
+
+		public static async Task SendAsync(GMailerOptions gMailerOptions, string recipient, string subject, string body)
+		{
+			var mailerOptions = GetMailerOptions(gMailerOptions);
+			await GenericMailer.SendAsync(mailerOptions, recipient, subject, body);
+		}
+
+		private static MailerOptions GetMailerOptions(GMailerOptions gMailerOptions)
+		{
+			return new()
 			{
 				SmtpHost = "smtp.gmail.com",
 				SmtpPort = 587,
@@ -21,8 +35,6 @@ namespace PortgateLib.Mailer
 				Email = $"{gMailerOptions.Username}@gmail.com",
 				Password = gMailerOptions.Password
 			};
-
-			GenericMailer.Send(mailerOptions, recipient, subject, body);
 		}
 	}
 }
