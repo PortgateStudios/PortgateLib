6a5177b908fa3bf98b1f05090c9fec3ec40565f2
Portgate Studios <info@portgatestudios.com>
2023-08-11 22:30:12 +0200

    feat(GenericMailer): SendAsync

--- Mailer/GenericMailer.cs
+++ Mailer/GenericMailer.cs
@@ -1,29 +1,54 @@
 using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
+
+// https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
+namespace System.Runtime.CompilerServices
+{
+	internal static class IsExternalInit { }
+}
 
 namespace PortgateLib.Mailer
 {
-	public class MailerOptions
+	public record MailerOptions
 	{
-		public string SmtpHost;
-		public int SmtpPort;
-		public bool EnableSsl;
-		public bool IsBodyHtml;
-		public string Name;
-		public string Email;
-		public string Password;
+		public string SmtpHost { get; init; }
+		public int SmtpPort { get; init; }
+		public bool EnableSsl { get; init; }
+		public bool IsBodyHtml { get; init; }
+		public string Name { get; init; }
+		public string Email { get; init; }
+		public string Password { get; init; }
 	}
 
 	public static class GenericMailer
 	{
 		public static void Send(MailerOptions mailerOptions, string recipient, string subject, string body)
 		{
+			using var client = CreateSmtpClient(mailerOptions);
+			var msg = CreateMailMessage(mailerOptions, recipient, subject, body);
+			client.Send(msg);
+		}
+
+		public static async Task SendAsync(MailerOptions mailerOptions, string recipient, string subject, string body)
+		{
+			using var client = CreateSmtpClient(mailerOptions);
+			var msg = CreateMailMessage(mailerOptions, recipient, subject, body);
+			await client.SendMailAsync(msg);
+		}
+
+		private static SmtpClient CreateSmtpClient(MailerOptions mailerOptions)
+		{
 			var client = new SmtpClient(mailerOptions.SmtpHost, mailerOptions.SmtpPort)
 			{
 				Credentials = new NetworkCredential(mailerOptions.Email, mailerOptions.Password),
-				EnableSsl = mailerOptions.EnableSsl,
+				EnableSsl = mailerOptions.EnableSsl
 			};
+			return client;
+		}
 
+		private static MailMessage CreateMailMessage(MailerOptions mailerOptions, string recipient, string subject, string body)
+		{
 			var msg = new MailMessage()
 			{
 				From = new MailAddress(mailerOptions.Email, mailerOptions.Name),
@@ -31,10 +56,8 @@ namespace PortgateLib.Mailer
 				Body = body,
 				IsBodyHtml = mailerOptions.IsBodyHtml
 			};
-
 			msg.To.Add(recipient);
-
-			client.Send(msg);
+			return msg;
 		}
 	}
 }
